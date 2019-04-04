ea0b3f98bdcae996a91984712824ad11e5c4de55
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-04-03 21:17:52 -0300

    Configure ansible

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-echo "Installing .dotfiles..."
+
+echo "Verifying ansible installation..."
+
+if [[ ! -x /usr/bin/ansible ]]; then
+  echo "Installing ansible..."
+  sudo pacman -S ansible --noconfirm
+fi
+
+ansible --version
+
+echo "Running playbook..."
+ansible-playbook -K ansible/main.yml
