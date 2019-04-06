d68d5fb11c5bda3bc3baa4724742d94df83cc60a
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-04-06 16:49:30 -0300

    Install ansible packer plugin and connman

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Verifying packer installation..."
+
+if [[ ! -x /usr/bin/packer ]]; then
+  echo "Installing packer..."
+  sudo pacman -S packer curl --noconfirm
+fi
+
 echo "Verifying ansible installation..."
 
 if [[ ! -x /usr/bin/ansible ]]; then
@@ -7,7 +14,13 @@ if [[ ! -x /usr/bin/ansible ]]; then
   sudo pacman -S ansible --noconfirm
 fi
 
-ansible --version
+echo "Verifying ansible plugin for packer..."
+
+if [[ ! -x /usr/share/ansible/plugins/modules/packer ]]; then
+  echo "Installing ansible-packer..."
+  sudo curl -fLo /usr/share/ansible/plugins/modules/packer --create-dirs https://raw.githubusercontent.com/austinhyde/ansible-packer/master/packer
+  sudo chmod -R 755 /usr/share/ansible/plugins
+fi
 
 echo "Running playbook..."
 ansible-playbook -K ansible/main.yml
