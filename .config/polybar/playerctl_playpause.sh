2c2c19091acdb53dc45d3a553e05acb504a417f3
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-08-05 14:20:43 -0300

    Fixes playerctl scripts

--- .config/polybar/playerctl_playpause.sh
+++ .config/polybar/playerctl_playpause.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl -p spotify status 2>/dev/null)
 
-if [[ $player_status = "Playing" ]]; then
-    echo ""
-elif [[ $player_status = "Paused" ]]; then
-    echo ""
+if [[ $player_status == "Playing" ]]; then
+  echo ""
+elif [[ $player_status == "Paused" ]]; then
+  echo ""
 fi
-
--- .config/polybar/playerctl_title.sh
+++ .config/polybar/playerctl_title.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl -p spotify status 2>/dev/null)
 if [[ $? -eq 0 ]]; then
-    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+  metadata="$(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
 fi
 
-# Foreground color formatting tags are optional
-if [[ $player_status = "Playing" ]]; then
-    echo "$metadata"      # Orange when playing
-elif [[ $player_status = "Paused" ]]; then
-    echo "Paused"      # Greyed out info when paused
+if [[ $player_status == "Playing" ]]; then
+  echo "$metadata"
+elif [[ $player_status == "Paused" ]]; then
+  echo "Paused"
 else
-    echo "Not connected" # Greyed out icon when stopped
+  echo "Not connected"
 fi
