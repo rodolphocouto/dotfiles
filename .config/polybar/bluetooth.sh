5061f8d10bedd89e59cbdf327ef190a05478a452
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2020-11-29 21:38:52 -0300

    Set polybar on multiple monitors

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
-then
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
   echo ""
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then
+  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then
     echo ""
   else
-    device=$(bluetoothctl devices | cut -c 26-100)
+    device=$(bluetoothctl paired-devices | cut -c 26-100)
     echo "%{F#66ffffff}%{F-} $device"
   fi
 fi
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,4 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
