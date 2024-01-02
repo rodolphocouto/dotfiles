fd5bfec453bd848fa1434788cfb101c3ed2a61c5
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2024-01-02 14:53:28 -0300

    Change headset id

--- .config/i3/headset.sh
+++ .config/i3/headset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $(pactl list cards | grep "Active Profile: headset-head-unit-msbc" | wc -c) -ne 0 ]; then
-  pactl set-card-profile bluez_card.14_3F_A6_E2_B5_A4 a2dp-sink-sbc
+  pactl set-card-profile bluez_card.88_C9_E8_79_D8_88 a2dp-sink-ldac
 else
-  pactl set-card-profile bluez_card.14_3F_A6_E2_B5_A4 headset-head-unit-msbc
+  pactl set-card-profile bluez_card.88_C9_E8_79_D8_88 headset-head-unit-msbc
 fi
